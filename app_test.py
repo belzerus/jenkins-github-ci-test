0d8fe1fe23143cd7086c084ba6594a2fd8097d69
Petter Mabäcker <petter.mabacker@smarteye.se>
2020-04-13 08:55:03 +0200

    app: Add proper formatting

--- app_test.py
+++ app_test.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
 import unittest
+
 import app
 
-class TestApp(unittest.TestCase):
 
+class TestApp(unittest.TestCase):
     def test_app_print_func(self):
         self.assertTrue(app.print_func("Test print func"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
